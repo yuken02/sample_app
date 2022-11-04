78573bf1e9011b6cccff1fd2e76d5ab6b7c0b840
yuken02 <git@github.com:yuken02/practice.git>
2022-11-04 07:14:39 +0000

    [Finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
+
+  get 'top' => 'homes#top'
+  resources :lists
+
+  # get 'lists/new'
+  # get '/top' => 'homes#top'
+  # post 'lists' => 'lists#create'
+  # get 'lists' => 'lists#index'
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'list/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
